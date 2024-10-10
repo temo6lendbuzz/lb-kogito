afca891e9d7beee633874fd640a623913f668791
Temo Monroy <temo.monroy@lendbuzz.com>
2024-10-10 13:23:48 -0400

    Update .gitignore to ignore /target directory

--- target/generated-sources/kogito/org/kie/kogito/app/ProcessCloudEventMetaFactory.java
+++
@@ -1,7 +0,0 @@
-package org.kie.kogito.app;
-
-import org.kie.kogito.event.cloudevents.CloudEventMeta;
-
-@org.springframework.context.annotation.Configuration()
-public class ProcessCloudEventMetaFactory {
-}
